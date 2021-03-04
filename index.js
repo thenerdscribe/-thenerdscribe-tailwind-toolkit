59d3333dde51f92a15aac282201a810b792a548f
Ryan Morton <morton.ryan92@gmail.com>
2021-03-04 11:04:28 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const plugin = require("tailwindcss/plugin");
+const colors = require("tailwindcss/colors");
+const _ = require("lodash");
+
+module.exports = function() {
+function({ addUtilities, theme }) {
+  const alternatingColors = (colors, offset) =>
+    _.map(colors, (colorSteps, name) => {
+      return _.map(colorSteps, (color, step) => ({
+        [`.alternating-${offset}-${name}${
+          _.isObject(colorSteps) && step !== "DEFAULT" ? `-${step}` : ""
+        } > *:nth-child(${offset})`]: {
+          "background-color": _.isObject(colorSteps) ? color : colorSteps,
+        },
+      }));
+    });
+  const themeColors = theme("colors", {});
+  const alternatingColorsOdd = alternatingColors(colors, "odd");
+  const alternatingColorsEven = alternatingColors(colors, "even");
+  const alternatingThemeOdd = alternatingColors(themeColors, "odd");
+  const alternatingThemeEven = alternatingColors(themeColors, "even");
+  addUtilities([
+    ...alternatingColorsOdd,
+    ...alternatingColorsEven,
+    ...alternatingThemeEven,
+    ...alternatingThemeOdd,
+  ]);
+}};
